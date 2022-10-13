7d9e24eb0f5ce9c759c7caf46bc64af38f5f2013
KrisPersson <krisperssonmusic@gmail.com>
2022-10-13 09:43:11 +0200

    Initial code

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log('A is bigger than B')
+} else {
+    console.log('A is not bigger than B')
+}
+
+a = 'apple';
+b = 'pear';
+
+if (a == b) {
+    console.log('A equals B')
+} else {
+    console.log('A does not equal B')
+}
+
+if (a != b) {
+    console.log('A does not equal B')
+} else {
+    console.log('A equals B')
+}
